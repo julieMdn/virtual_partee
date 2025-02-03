3868fa722cc5cdde4475c687cbb560bb912a5dd6
julieMdn <julie.midon@gmail.com>
2025-02-03 17:53:53 +0100

    admin js configurated

---
+++ src/app/api/admin/config.js
@@ -0,0 +1,30 @@
+import AdminJS from "adminjs";
+import { Database, Resource } from "@adminjs/prisma";
+import { prisma } from "@/lib/prisma"; // Ajustez le chemin selon votre configuration
+
+AdminJS.registerAdapter({ Database, Resource });
+
+const resources = [
+  { resource: { model: prisma.user, client: prisma }, options: {} },
+  { resource: { model: prisma.address, client: prisma }, options: {} },
+  { resource: { model: prisma.offer, client: prisma }, options: {} },
+  { resource: { model: prisma.timeSlot, client: prisma }, options: {} },
+  { resource: { model: prisma.booking, client: prisma }, options: {} },
+  { resource: { model: prisma.payment, client: prisma }, options: {} },
+  { resource: { model: prisma.score, client: prisma }, options: {} },
+  { resource: { model: prisma.course, client: prisma }, options: {} },
+];
+
+export const adminJs = new AdminJS({
+  databases: [prisma],
+  resources: resources,
+  rootPath: "/admin",
+  dashboard: {
+    component: AdminJS.bundle("./components/dashboard"),
+  },
+  branding: {
+    companyName: "Virtual Partee",
+    logo: false,
+    softwareBrothers: false,
+  },
+});
