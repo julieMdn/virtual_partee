04783f86416c632ff47e63d61d696bbe8261fde7
julieMdn <julie.midon@gmail.com>
2025-02-06 10:02:42 +0100

    fix home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main>
       <h1>Bienvenue</h1>
-      <a href="/admin" className="mt-4 underline">
-        Accéder à l'administration
-      </a>
+      <a href="/admin">Accéder à l'administration</a>
     </main>
   );
 }
